e882ad9cadfac19bbf268ba7348aa5b1de868996
cdh <jaehoon92@gmail.com>
2020-09-29 13:49:53 +0900

    first commit

---
+++ src/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <nav className="header">
+      {/* logo on the left -> img */}
+      <Link to="/">
+        <img
+          className="header__logo"
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+          alt=""
+        />
+      </Link>
+
+      {/* Search box */}
+      {/* 3 Links */}
+      {/* Basket icon with number */}
+    </nav>
+  );
+}
+
+export default Header;
